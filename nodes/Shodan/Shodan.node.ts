317f0758971d21ce3fa43980b8b74c9b044b1674
duong22 <thaiduonghp97@gmail.com>
2025-11-19 16:46:18 -0800

    fix name DNS display

--- nodes/Shodan/Shodan.node.ts
+++ nodes/Shodan/Shodan.node.ts
@@ -43,7 +43,7 @@ export class Shodan implements INodeType {
                         value: 'search',
                     },
                     {
-                        name: 'DN',
+                        name: 'DNS',
                         value: 'dns',
                     },
                     {
